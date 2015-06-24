e4148bbb7191038a1def0f9c48ef1c5320058195
Przemyslaw Kaminski <pkaminski@mirantis.com>
2015-06-24 09:48:51 +0200

    Pecan API draft added

---
+++ solar/solar/api/v1/controllers/root.py
@@ -0,0 +1,30 @@
+from pecan import expose, redirect
+from pecan.rest import RestController
+
+from webob.exc import status_map
+
+
+class RootControllerOld(object):
+
+    @expose(generic=True, template='index.html')
+    def index(self):
+        return dict()
+
+    @index.when(method='POST')
+    def index_post(self, q):
+        redirect('http://pecan.readthedocs.org/en/latest/search.html?q=%s' % q)
+
+    @expose('error.html')
+    def error(self, status):
+        try:
+            status = int(status)
+        except ValueError:  # pragma: no cover
+            status = 500
+        message = getattr(status_map.get(status), 'explanation', '')
+        return dict(status=status, message=message)
+
+
+class RootController(RestController):
+    @expose(template='json')
+    def get(self):
+        return {'hello': 'world'}
